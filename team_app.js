06734b59aa41637694a888f5138678292be7c498
Nikola <88djony@gmail.com>
2021-05-14 15:30:34 -0700

    start inquirer prompt function

--- team_app.js
+++ team_app.js
@@ -4,3 +4,27 @@ const fs = require("fs");
 const Manager = require();
 const Engineer = require();
 const Intern = require();
+
+
+function startApp() {
+    createHTML();
+    addWorker();
+}
+
+function addWorker() {
+    inquirer.prompt([{
+        message: "Enter worker's name.",
+        name: "name"
+    },
+{
+    type: "list",
+    message: "Select worker's role.",
+    choices: [
+        "Manager",
+        "Engineer",
+        "Intern"
+    ],
+    name: "role"
+},
+])
+}
