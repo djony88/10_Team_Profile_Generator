592da1869e412de7f333248718b43d9f7c461fd7
Nikola <88djony@gmail.com>
2021-05-15 15:23:05 -0700

    add manager.js

---
+++ library/manger.js
@@ -0,0 +1,16 @@
+const worker = require("./");
+
+class Manager extends {
+    constructor (name, ID, officeNum, email) {
+        super (name, ID, email);
+        this.officeNum = officeNum;
+    }
+    getPosition() {
+        return "Manager";
+    };
+    getOfficeNum() {
+        return this.officeNum;
+    }
+}
+
+module.exports = Manager;
